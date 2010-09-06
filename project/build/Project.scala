0a3085d8b7b8c5592cfecb30ccf84421839c88b3
Luke Amdor <luke.amdor@gmail.com>
2010-09-06 18:07:31 -0500

    Setting trace off

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -3,5 +3,7 @@ import sbt._
 class Project(info: ProjectInfo) extends DefaultProject(info) {
   val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
 
+  log.setTrace(-1)
+
   lazy val koans = testOnlyAction(Array("org.scalakoans.Koans")) describedAs "Run through the Scala Koans"
 }
