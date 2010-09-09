07d06692d63ab9fb9a33b29839791503c02abbe1
Luke Amdor <luke.amdor@gmail.com>
2010-09-09 07:53:52 -0500

    Stubs for AboutClasses

---
+++ src/test/scala/org/scalakoans/AboutClasses.scala
@@ -0,0 +1,28 @@
+package org.scalakoans
+
+import support.KoanSuite
+
+class AboutClasses extends KoanSuite {
+
+  koan ("Class") {
+  }
+
+  koan ("Class getters") {
+  }
+
+  koan ("Class getter/setters") {
+  }
+
+  koan ("Class private fields") {
+  }
+
+  koan ("Class explict getter and setters") {
+  }
+
+  koan ("Class instance methods") {
+  }
+
+  koan ("Class super class constructors") {
+  }
+
+}
