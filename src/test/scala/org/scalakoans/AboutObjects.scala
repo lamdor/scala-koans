61b383d8f42d675a65324d2fbb58f4267fdb865f
Luke Amdor <luke.amdor@gmail.com>
2010-09-13 22:28:37 -0500

    Added koans about singleton objects

---
+++ src/test/scala/org/scalakoans/AboutObjects.scala
@@ -0,0 +1,37 @@
+package org.scalakoans
+
+import support.KoanSuite
+
+class AboutObjects extends KoanSuite {
+
+  koan ("singleton objects") {
+    object Thing {
+      def add2(i: Int) = i + 2
+    }
+
+    Thing.add2(2) should __
+
+    meditate
+    // Thing.add3(4) should be(7)
+  }
+
+  koan ("singleton objects can extend classes") {
+    class Cat(val name: String)
+
+    object LouFerigno extends Cat("Lou")
+    LouFerigno.name should __
+
+    meditate
+    // LouFerigno.meow should be("meow")
+  }
+
+  koan ("Singleton objects are good for constants") {
+    object Constants {
+      val RED = "RED"
+      val GREEN = "green"
+    }
+
+    Constants.RED should __
+  }
+
+}
